a67a577b434decd50770f576c5dc9a3cfa1af6c7
John Vehmeyer <j.vehmeyer@gmail.com>
2021-07-06 15:57:22 -0500

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -38,4 +38,58 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    // console.log("POST connected")
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        const { title, contents } = req.body
+        Posts.insert({ title, contents })
+            .then(posts => {
+                res.status(201).json(posts)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database"
+            })
+        })
+    }
+})
+
+router.put('/:id', (req, res) => {
+    // console.log("PUT connected")
+
+    if (!req.params.id) {
+        res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+    } else if (!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        // const { title, contents } = req.body
+        const changes = req.body
+        Posts.update(req.params.id, changes)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The post information could not be modified"
+            })
+        })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    // console.log("DELETE connected")
+})
+
+
+
 module.exports = router
