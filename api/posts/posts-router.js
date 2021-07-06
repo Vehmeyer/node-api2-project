1bbe16f193b242b42f6b3945ff426ed542d48e78
John Vehmeyer <j.vehmeyer@gmail.com>
2021-07-06 15:04:46 -0500

    created get request to fetch post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,4 +18,24 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    // console.log("GET by ID connected")
+    Posts.findById(req.params.id)
+        .then(posts => {
+            if (posts) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            })
+        })
+})
+
 module.exports = router
